1fa349f366773c0a5cf2f6dc6e42998927b668c5
gpsuperleague <gp.superleague@gmail.com>
2026-04-01 22:54:40 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD1bGkjhR5QHYjlOZ4DPADMir_-W8O6Qsk",
+    authDomain: "gpsl-31bb8.firebaseapp.com",
+    projectId: "gpsl-31bb8",
+    storageBucket: "gpsl-31bb8.firebasestorage.app",
+    messagingSenderId: "313840138087",
+    appId: "1:313840138087:web:8134a9fc396247dd7421c2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
