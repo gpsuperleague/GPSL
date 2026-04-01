1d795b33258e5972e2e29fa5c89042833895b87e
gpsuperleague <gp.superleague@gmail.com>
2026-04-01 22:57:41 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,13 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyD1bGkjhR5QHYjlOZ4DPADMir_-W8O6Qsk"",
+  authDomain: "gpsl-31bb8.firebaseapp.com",
+  projectId: "gpsl-31bb8",
+  storageBucket: "gpsl-31bb8.firebasestorage.app",
+  messagingSenderId: "313840138087",
+  appId: "1:313840138087:web:8134a9fc396247dd7421c2"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyD1bGkjhR5QHYjlOZ4DPADMir_-W8O6Qsk",
-    authDomain: "gpsl-31bb8.firebaseapp.com",
-    projectId: "gpsl-31bb8",
-    storageBucket: "gpsl-31bb8.firebasestorage.app",
-    messagingSenderId: "313840138087",
-    appId: "1:313840138087:web:8134a9fc396247dd7421c2"
-  };
+firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const auth = firebase.auth();
+const db = firebase.firestore();
