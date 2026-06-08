02de1f5f6705123024defcbfbc8e56a5bac8f2bd
gpsuperleague <gp.superleague@gmail.com>
2026-06-08 14:56:16 +0100

    update

--- global.js
+++ global.js
@@ -705,7 +705,7 @@ export async function buildNav() {
   let firstActiveNavSectionId;
   let normalizeNavPath;
   try {
-    const navMod = await import("./nav_config.js?v=20250602-admin-cup-prizes");
+    const navMod = await import("./nav_config.js?v=20250602-cup-prizes-submenu");
     NAV_SECTIONS = navMod.NAV_SECTIONS;
     ADMIN_NAV_SECTION = navMod.ADMIN_NAV_SECTION;
     isNavItemActive = navMod.isNavItemActive;
